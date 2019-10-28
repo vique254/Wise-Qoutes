a856e130bb3a28a8d1d3f867351b6123cb8407de
vique254 <victoronyango051@gmail.com>
2019-10-28 11:54:06 +0300

    add new contents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-
+import { Quote } from './quote';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:string[];
-  constructor(){
-  this.quotes = ['Never let your anger till the sun sets','East or West home is the BEST!', 'It is never too late to start again']
-  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteDetailComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
