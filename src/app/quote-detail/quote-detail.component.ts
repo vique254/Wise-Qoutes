f47d6a5924417dc595f71b7c3b4d13df9afb7b6a
vique254 <victoronyango051@gmail.com>
2019-10-28 15:15:33 +0300

    create eventemmitter

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 @Component({
   selector: 'app-quote-detail',
@@ -7,6 +7,11 @@ import { Quote } from '../quote';
 })
 export class QuoteDetailComponent implements OnInit {
 @Input() quote:Quote;
+@Output() isComplete = new EventEmitter<boolean>();
+
+quoteComplete(complete:boolean){
+  this.isComplete.emit(complete);
+}
   constructor() { }
 
   ngOnInit() {
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,16 +7,22 @@ import {Quote} from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    {id:1, name:'Life',description:'Never let your anger till the sun sets for if you do you will not see tomorrow.'},
-    {id:2,name:'Home',description:'East or West home is the BEST!'},
-    {id:3,name:'Start over',description:'It is never too late to start again'},
+    {id:1, name:'Life',description:'"Life can only be understood backwards, but it must be lived forward.",  – V.i.q.u.e'},
+    {id:2,name:'Home',description:'“A house is made with walls and beams. A home is made with love and dreams.” – Unknown!'},
+    {id:3,name:'Start over',description:'It is never too late to start again!!'},
     {id:4,name:'Future',description:'The best thing about the future is that it comes only one day at a time.'},
-    {id:5,name:'Differences',description:'Women and foxes, being weak, are distinguished by superior tact'},
+    {id:5,name:'Differences',description:'Women and foxes, being weak, are distinguished by superior tact!'},
     {id:6,name:'Finish',description:'It does not matter how slowly you go, so long as you do not stop.'},
   ];
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
+  completeQuote(isComplete, index){
+    if (isComplete) {
+      this.quotes.splice(index,1);
+    }
+  }
+  
   constructor() { }
 
   ngOnInit() {
